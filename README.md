ca9a222f0d88623469a2643457738951ef04da55
Jordan Taylor <jtaylor007.jt@gmail.com>
2016-10-17 11:07:01 -0400

    Initial sentiment analysis commit with code

---
+++ README.md
@@ -0,0 +1,16 @@
+# Sentiment Tweet Analysis Challenge
+
+This repository is in response to Siraj's youtube/github challenge.
+[Sentiment Analysis Challenge](https://github.com/llSourcell/twitter_sentiment_challenge)
+
+The script will search for a keyword in tweets and run sentiment analysis to determine the emotion of the tweet's content.
+
+The results will write to a small csv file for analysis based on the phrase _"Mondays"_ -- because everyone loves Mondays!!
+
+The python modules used:
+
+- **Numpy**
+- **TextBlob**
+- **Tweepy**å
+
+Note: must have twitter API credentials to run the python script
---
+++ demo.py
@@ -0,0 +1,48 @@
+import csv
+import tweepy
+import numpy as np
+from textblob import TextBlob
+
+# credentials
+consumer_key = ''
+consumer_secret = ''
+
+access_token = ''
+access_token_secret = ''
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Mondays')
+total_tweets = len(public_tweets)
+
+sentiment = np.chararray((1, total_tweets), itemsize=15)
+emotion = np.empty([1, total_tweets])
+subjective = np.empty([1, total_tweets])
+
+for i in range(total_tweets):
+    analysis = TextBlob(public_tweets[i].text)
+    emotion[0, i] = analysis.sentiment.polarity
+    subjective[0, i] = analysis.sentiment.subjectivity
+    if emotion [0, i] < 0:
+        sentiment[0, i] = 'Negative'
+    elif emotion [0, i] == 0:
+        sentiment[0, i] ='Neutral'
+    else:
+        sentiment[0, i] = 'Positive'
+
+
+with open('sentiment_analysis.csv', 'w') as csvfile:
+    fieldnames = ['Tweet', 'Sentiment', 'Emotion', 'Subjectivity']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for i in range(total_tweets):
+        writer.writerow({
+            'Tweet':(public_tweets[i].text).encode('utf-8'),
+            'Sentiment': str(sentiment[0, i]),
+            'Emotion': str(emotion[0, i])
+            'Subjectivity': str(subjective[0, i])
+        })
